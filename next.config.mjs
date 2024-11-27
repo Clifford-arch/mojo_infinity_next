a740a7c9c486c2ee16aa20314e93b9c5a1964ac2
Clifford <cliffordchristophermarx@gmail.com>
2024-11-27 12:13:15 +0530

    redirect

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/mojo-infinity",
+        destination: "/landing",
         permanent: true,
       },
     ];
