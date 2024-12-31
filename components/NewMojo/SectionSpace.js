74d7fe1e194ca00c48b537d8a88f12c8c3d94633
Clifford <cliffordchristophermarx@gmail.com>
2024-12-31 17:03:32 +0530

    Content changes in swiper of mojo infinity

--- components/NewMojo/SectionSpace.js
+++ components/NewMojo/SectionSpace.js
@@ -7,10 +7,107 @@ const SectionSpace = () => {
     all: {
       title: "All Portfolios",
       content: (
-        <p>
-          All Portfolio Content. This will have data related to All Portfolio
-          type.
-        </p>
+        <>
+          <div className="explore-text">Explore Other Portfolios</div>
+          <div className="row justify-content-center pt-3">
+            <div className="col-md-4 col-12 pb-3">
+              <div className="inner-card-style d-flex flex-column h-100 w-100">
+                <div className="conservative-portfolio">
+                  Conservative Portfolio
+                </div>
+                <div className="risk-text">Low Risk</div>
+                <div className="para-font">
+                  The aim is steady growth with minimal risk, making it ideal
+                  for individuals who prioritize capital preservation and are
+                  risk-averse.
+                </div>
+                <div className="fixed-footer">
+                  <div className="cards-flex-style">
+                    <div className="flex-div-1">
+                      <div className="investment-stock">3Y CAGR</div>
+                      <div className="investment-value">12%</div>
+                    </div>
+                    <div className="flex-div-2">
+                      <div className="investment-stock">Min. Investment</div>
+                      <div className="investment-value">2,00,000</div>
+                    </div>
+                  </div>
+                  <div className="text-center pt-3">
+                    <button
+                      type="button"
+                      className="btn btn-lg btn-primary w-100"
+                    >
+                      Know More
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-12 pb-3">
+              <div className="inner-card-style d-flex flex-column h-100 w-100">
+                <div className="trending">
+                  <div className="conservative-portfolio">
+                    Aggressive Portfolio
+                  </div>
+                  <div className="trending-div">TRENDING</div>
+                </div>
+                <div className="risk-text">High Risk</div>
+                <div className="para-font">
+                  The aim is high growth through investments in mid-cap and
+                  small-cap stocks, making it ideal for investors with a
+                  high-risk tolerance and a long investment horizon.
+                </div>
+                <div className="cards-flex-style">
+                  <div className="flex-div-1">
+                    <div className="investment-stock">3Y CAGR</div>
+                    <div className="investment-value">12%</div>
+                  </div>
+                  <div className="flex-div-2">
+                    <div className="investment-stock">Min. Investment</div>
+                    <div className="investment-value">2,00,000</div>
+                  </div>
+                </div>
+                <div className="text-center pt-3">
+                  <button
+                    type="button"
+                    className="btn btn-lg btn-primary w-100"
+                  >
+                    Know More
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-12 pb-3">
+              <div className="inner-card-style d-flex flex-column h-100 w-100">
+                <div className="conservative-portfolio">Moderate Portfolio</div>
+                <div className="risk-text">Medium Risk</div>
+                <div className="para-font">
+                  The aim is a balance between growth and stability, making it
+                  suitable for individuals with a medium risk tolerance and a
+                  focus on long-term returns
+                </div>
+                <div className="cards-flex-style">
+                  <div className="flex-div-1">
+                    <div className="investment-stock">3Y CAGR</div>
+                    <div className="investment-value">12%</div>
+                  </div>
+                  <div className="flex-div-2">
+                    <div className="investment-stock">Min. Investment</div>
+                    <div className="investment-value">2,00,000</div>
+                  </div>
+                </div>
+                <div className="text-center pt-3">
+                  <button
+                    type="button"
+                    className="btn btn-lg btn-primary w-100"
+                  >
+                    Know More
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
       ),
     },
     "multi-bagger": {
@@ -25,7 +122,7 @@ const SectionSpace = () => {
                     <div className="d-flex align-items-center gap-3 mb-2">
                       <span className="mp-sectorIcons flex-shrink-0">
                         <img
-                          src="https://sandbox-st.mojomf.com/images/mf/logos/icici_mm.png"
+                          src="https://sandbox-st.mojowealth.in/images/multi-bagger-portfolio-icon.png"
                           alt="icici_mm.png"
                         />
                       </span>
@@ -67,7 +164,7 @@ const SectionSpace = () => {
                   </div>
                   <div className="col-12 col-md-4 wealth-basket-btn">
                     <img
-                      src="https://sandbox-st.mojowealth.in/images/home/graph1a.png"
+                      src="https://sandbox-st.mojowealth.in/images/home/graph1a-lock.png"
                       className="img-fluid"
                       alt="graph1a.png"
                     />
@@ -144,28 +241,188 @@ const SectionSpace = () => {
     aggressive: {
       title: "Aggressive",
       content: (
-        <p>
-          Aggressive Portfolio Content. This will have data related to
-          Aggressive portfolio type.
-        </p>
+        <div className="col-12 mt-3">
+          <div className="mp-card">
+            <div className="card-body">
+              <div className="row align-items-start">
+                <div className="col-12 col-md-7 me-md-4 wealth-basket">
+                  <div className="d-flex align-items-center gap-3 mb-2">
+                    <span className="mp-sectorIcons flex-shrink-0">
+                      <img
+                        src="https://sandbox-st.mojowealth.in/images/aggressive-portfolio-icon.png"
+                        alt="icici_mm.png"
+                      />
+                    </span>
+                    <div className="flex-grow-1 title">
+                      Aggressive Portfolio <div className="tag"> High Risk</div>
+                    </div>
+                  </div>
+                  <div>
+                    <p>
+                      The aim is maximum capital appreciation through high-risk
+                      investments, making it suitable for individuals with a
+                      very high-risk tolerance and a long-term perspective.
+                    </p>
+                    <div className="info row mb-3">
+                      <div className="col-4 col-md-3 ps-0">
+                        <div className="head">Min. Investment</div>
+                        <div className="value">2,00,000</div>
+                      </div>
+                      <div className="col-5 col-md-4">
+                        <div className="head">No. of Stocks</div>
+                        <div className="value">10-15</div>
+                      </div>
+                      <div className="col-3 col-md-3">
+                        <div className="head">3 Year CAGR</div>
+                        <div className="value">
+                          <img
+                            src="https://sandbox-st.mojowealth.in/images/home/carg.png"
+                            alt="carg.png"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <button type="button" className="btn btn-primary">
+                    Know More
+                  </button>
+                </div>
+                <div className="col-12 col-md-4 wealth-basket-btn">
+                  <img
+                    src="https://sandbox-st.mojowealth.in/images/home/graph1a-lock.png"
+                    className="img-fluid"
+                    alt="graph1a.png"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       ),
     },
     moderate: {
       title: "Moderate",
       content: (
-        <p>
-          Moderate Portfolio Content. This will have data related to Moderate
-          portfolio type.
-        </p>
+        <div className="col-12 mt-3">
+          <div className="mp-card">
+            <div className="card-body">
+              <div className="row align-items-start">
+                <div className="col-12 col-md-7 me-md-4 wealth-basket">
+                  <div className="d-flex align-items-center gap-3 mb-2">
+                    <span className="mp-sectorIcons flex-shrink-0">
+                      <img
+                        src="https://sandbox-st.mojowealth.in/images/moderate-portfolio-icon.png"
+                        alt="icici_mm.png"
+                      />
+                    </span>
+                    <div className="flex-grow-1 title">
+                      Moderate Portfolio <div className="tag"> High Risk</div>
+                    </div>
+                  </div>
+                  <div>
+                    <p>
+                      The aim is maximum capital appreciation through high-risk
+                      investments, making it suitable for individuals with a
+                      very high-risk tolerance and a long-term perspective.
+                    </p>
+                    <div className="info row mb-3">
+                      <div className="col-4 col-md-3 ps-0">
+                        <div className="head">Min. Investment</div>
+                        <div className="value">2,00,000</div>
+                      </div>
+                      <div className="col-5 col-md-4">
+                        <div className="head">No. of Stocks</div>
+                        <div className="value">10-15</div>
+                      </div>
+                      <div className="col-3 col-md-3">
+                        <div className="head">3 Year CAGR</div>
+                        <div className="value">
+                          <img
+                            src="https://sandbox-st.mojowealth.in/images/home/carg.png"
+                            alt="carg.png"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <button type="button" className="btn btn-primary">
+                    Know More
+                  </button>
+                </div>
+                <div className="col-12 col-md-4 wealth-basket-btn">
+                  <img
+                    src="https://sandbox-st.mojowealth.in/images/home/graph1a-lock.png"
+                    className="img-fluid"
+                    alt="graph1a.png"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       ),
     },
     conservative: {
       title: "Conservative",
       content: (
-        <p>
-          Conservative Portfolio Content. This will have data related to
-          Conservative portfolio type.
-        </p>
+        <div className="col-12 mt-3">
+          <div className="mp-card">
+            <div className="card-body">
+              <div className="row align-items-start">
+                <div className="col-12 col-md-7 me-md-4 wealth-basket">
+                  <div className="d-flex align-items-center gap-3 mb-2">
+                    <span className="mp-sectorIcons flex-shrink-0">
+                      <img
+                        src="https://sandbox-st.mojowealth.in/images/conservative-portfolio-icon.png"
+                        alt="icici_mm.png"
+                      />
+                    </span>
+                    <div className="flex-grow-1 title">
+                      Conservative Portfolio{" "}
+                      <div className="tag"> High Risk</div>
+                    </div>
+                  </div>
+                  <div>
+                    <p>
+                      The aim is maximum capital appreciation through high-risk
+                      investments, making it suitable for individuals with a
+                      very high-risk tolerance and a long-term perspective.
+                    </p>
+                    <div className="info row mb-3">
+                      <div className="col-4 col-md-3 ps-0">
+                        <div className="head">Min. Investment</div>
+                        <div className="value">2,00,000</div>
+                      </div>
+                      <div className="col-5 col-md-4">
+                        <div className="head">No. of Stocks</div>
+                        <div className="value">10-15</div>
+                      </div>
+                      <div className="col-3 col-md-3">
+                        <div className="head">3 Year CAGR</div>
+                        <div className="value">
+                          <img
+                            src="https://sandbox-st.mojowealth.in/images/home/carg.png"
+                            alt="carg.png"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <button type="button" className="btn btn-primary">
+                    Know More
+                  </button>
+                </div>
+                <div className="col-12 col-md-4 wealth-basket-btn">
+                  <img
+                    src="https://sandbox-st.mojowealth.in/images/home/graph1a-lock.png"
+                    className="img-fluid"
+                    alt="graph1a.png"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       ),
     },
   };
